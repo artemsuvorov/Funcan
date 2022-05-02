fa9f8fb315a01a0b285dd005095031884d61b75a
Arista213 <kvvfum@gmail.com>
2022-05-02 16:11:27 +0500

    Change implicit function to explicit function

--- Controllers/FunctionInputController.cs
+++ Controllers/FunctionInputController.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq.Expressions;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Plotly.NET;
+using Expression = NCalc.Expression;
 
 namespace Funcan.Controllers
 {
@@ -22,33 +25,57 @@ namespace Funcan.Controllers
         }
 
         [HttpGet]
-        public string GetFunction(string input)
+        public ContentResult GetFunction(string input)
         {
             var validatedFunction  = ValidateInputFunction(input);
             var expression         = new NCalc.Expression(validatedFunction);
             var functionExpression = CreateExpression(expression);
-            var f                  = functionExpression.Compile();
-            var printer = new FunctionPainter(f);
-            printer.Paint();
-            return validatedFunction;
+            Draw(functionExpression);
+
+            Console.WriteLine(validatedFunction);
+            return new ContentResult
+            {
+                ContentType = "text/html",
+                Content     = File.ReadAllText(@"Functions/function.html")
+            };
         }
 
         private static string ValidateInputFunction(string input)
         {
-            var regex = new Regex(@"(\((?>\((?<c>)|[^()]+|\)(?<-c>))*(?(c)(?!))\)|\w)\^(\((?>\((?<c>)|[^()]+|\)(?<-c>))*(?(c)(?!))\)|\w)");
+            var regex = new Regex(@"(\((?>\((?<c>)|[^()]+|\)(?<-c>))*(?(c)(?!))\)|\w+)\^(\((?>\((?<c>)|[^()]+|\)(?<-c>))*(?(c)(?!))\)|\w+)");
             return regex.Replace(input, match => $"Pow({match.Groups[1]},{match.Groups[2]})");
         }
 
-        private static Expression<Func<double, double, double>> CreateExpression(NCalc.Expression expression)
+        private static Expression<Func<double, double>> CreateExpression(NCalc.Expression expression)
         {
-            Func<double, double, double> function = (x, y) =>
+            Func<double, double> function = x =>
             {
                 expression.Parameters["x"] = x;
-                expression.Parameters["y"] = y;
                 return double.Parse(expression.Evaluate().ToString() ?? throw new InvalidOperationException());
             };
 
-            return (x, y) => function(x, y);
+            return x => function(x);
+        }
+
+        private static void Draw(Expression<Func<double, double>> expression)
+        {
+            var points = MakePoints(expression.Compile());
+            var chart  = Chart2D.Chart.Spline<double, double, string>(points);
+            chart.WithXAxisStyle(Title.init("xAxis"), ShowGrid: false, ShowLine: true)
+                .WithYAxisStyle(Title.init("yAxis"), ShowGrid: false, ShowLine: true)
+                .SaveHtml(@"Functions/function");
+        }
+
+        private static IEnumerable<Tuple<double, double>> MakePoints(Func<double, double> function)
+        {
+            var points = new List<Tuple<double, double>>();
+            for (double x = -100; x <= 100; x += 0.001f)
+            {
+                var y = function(x);
+                points.Add(Tuple.Create(x, y));
+            }
+
+            return points;
         }
     }
 }
--- Controllers/FunctionPainter.cs
+++ Controllers/FunctionPainter.cs
@@ -8,36 +8,30 @@ namespace Funcan.Controllers
 {
     public class FunctionPainter
     {
-        private readonly Func<double, double, double> _mathFunc;
-        private static readonly double Epsilon = Math.Pow(Math.E, -3);
+        private readonly        Func<double, double> _mathFunc;
+        private static readonly double               Epsilon = Math.Pow(Math.E, -3);
 
-        public FunctionPainter(Func<double, double, double> mathFunc)
+        public FunctionPainter(Func<double, double> mathFunc)
         {
             _mathFunc = mathFunc;
         }
 
         public void Paint()
         {
-            //var xy = (x, y);
             var points = GetPoints();
-            var tuples = new List<Tuple<double, double>>()
-                {new Tuple<double, double>(1, 1), new Tuple<double, double>(2, 4), new Tuple<double, double>(3, 9)};
-            var chart = Chart2D.Chart.Spline<double, double, string>(points);
+            var chart  = Chart2D.Chart.Spline<double, double, string>(points);
             chart.WithXAxisStyle(title: Title.init("xAxis"), ShowGrid: false, ShowLine: true)
                 .WithYAxisStyle(title: Title.init("yAxis"), ShowGrid: false, ShowLine: true)
-                .Show();
+                .SaveHtml(@"Functions/function");
         }
 
-        public IEnumerable<Tuple<double, double>> GetPoints()
+        private IEnumerable<Tuple<double, double>> GetPoints()
         {
-            List<Tuple<double, double>> points = new List<Tuple<double, double>>();
-            for (double x = -2; x <= 2; x+=0.05f)
-            for (double y = 0; y <= 2; y += 0.05f)
+            var points = new List<Tuple<double, double>>();
+            for (double x = -100; x <= 100; x += 0.001f)
             {
-                var result = _mathFunc(x, y);
-                Console.WriteLine(result);
-                if (Math.Abs(result) < Epsilon)
-                    points.Add(Tuple.Create(x, y));
+                var y = _mathFunc(x);
+                points.Add(Tuple.Create(x, y));
             }
 
             return points;
