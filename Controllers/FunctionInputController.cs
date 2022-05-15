ec7bda81dc1a0094c8f89ef292a4024a2a1cb89e
Arista213 <kvvfum@gmail.com>
2022-05-15 15:07:56 +0500

    delete plotly.net

--- Controllers/FunctionInputController.cs
+++ Controllers/FunctionInputController.cs
@@ -1,15 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Plotly.NET;
-using Expression = NCalc.Expression;
 
 namespace Funcan.Controllers
 {
@@ -29,24 +23,16 @@ namespace Funcan.Controllers
         [HttpGet]
         public ContentResult GetFunction(string input)
         {
-            var validatedFunction  = ValidateInputFunction(input);
-            var expression         = new NCalc.Expression(validatedFunction);
+            var expression         = new NCalc.Expression(input);
             var functionExpression = CreateExpression(expression);
             // Draw(functionExpression);
             var points = MakePoints(functionExpression.Compile());
 
-            Console.WriteLine(validatedFunction);
             return new ContentResult
             {
                 ContentType = "text/html",
                 Content     = points.GetJsonBody()
             };
-        }
-
-        private static string ValidateInputFunction(string input)
-        {
-            var regex = new Regex(@"(\((?>\((?<c>)|[^()]+|\)(?<-c>))*(?(c)(?!))\)|\w+)\^(\((?>\((?<c>)|[^()]+|\)(?<-c>))*(?(c)(?!))\)|\w+)");
-            return regex.Replace(input, match => $"Pow({match.Groups[1]},{match.Groups[2]})");
         }
 
         private static Expression<Func<double, double>> CreateExpression(NCalc.Expression expression)
@@ -59,15 +45,6 @@ namespace Funcan.Controllers
 
             return x => function(x);
         }
-
-        // private static void Draw(Expression<Func<double, double>> expression)
-        // {
-        //     var points = MakePoints(expression.Compile());
-        //     var chart  = Chart2D.Chart.Spline<double, double, string>(points);
-        //     chart.WithXAxisStyle(Title.init("xAxis"), ShowGrid: false, ShowLine: true)
-        //         .WithYAxisStyle(Title.init("yAxis"), ShowGrid: false, ShowLine: true)
-        //         .SaveHtml(@"Functions/function");
-        // }
 
         private static PointSet MakePoints(Func<double, double> function)
         {
