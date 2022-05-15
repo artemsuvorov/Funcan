e6103045fb484c740ff9c0025e5034c78b20b3b2
3008 <sufonic@gmail.com>
2022-05-15 21:15:33 +0500

    Implemented drawing functions on InputController Реализована отрисовка графика функции с помощью plotly.js без поддержки стилей

---
+++ wwwroot/script/input-func-draw.js
@@ -0,0 +1,54 @@
+﻿var chartContainer = document.getElementById("chart-container");
+
+var layout = {
+    title: "Chart",
+    width: 500,
+    height: 350,
+    xaxis: {
+        title: "X Axis",
+        zeroline: false
+    },
+    yaxis: {
+        title: "Y Axis",
+        showline: false
+    },
+};
+
+function assembleChartData(points) {
+    var chartData = {
+        x: [],
+        y: []
+    };
+
+    for (var point of points) {
+        chartData.x.push(point.X);
+        chartData.y.push(point.Y);
+    }
+
+    return [ chartData ];
+}
+
+function drawFunc(data) {
+    var points = data[0].Points;
+    var chartData = assembleChartData(points);
+    Plotly.newPlot(chartContainer, chartData, layout);
+}
+
+function submitInputFunc(event) {
+    event.preventDefault();
+
+    var inputFunc = event.target.elements.input.value;
+    var params = { input: inputFunc };
+
+    var target = event.target.action + "?" + new URLSearchParams(params);
+    fetch(target)
+        .then(
+            (response) => response.json(),
+            (error) => console.error(error)
+        )
+        .then(
+            (data) => drawFunc(data)
+        );
+
+    return false;
+}
