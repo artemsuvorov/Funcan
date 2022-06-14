37a0c512189e47dddf7d1fb580d911eb858cb062
Arista213 <kvvfum@gmail.com>
2022-06-14 21:27:43 +0500

    Add thread safety in history repository

--- Funcan/Domain/Repository/MemoryHistoryRepository.cs
+++ Funcan/Domain/Repository/MemoryHistoryRepository.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using Funcan.Domain.Models;
@@ -6,15 +7,23 @@ namespace Funcan.Domain.Repository;
 
 public class MemoryHistoryRepository : IHistoryRepository
 {
-    private Dictionary<int, Dictionary<string, HistoryEntry>> HistoryEntries { get; } = new();
+    private ConcurrentDictionary<int, ConcurrentDictionary<string, HistoryEntry>> HistoryEntries { get; } = new();
 
     public void Save(int userId, HistoryEntry historyEntry)
     {
         if (!HistoryEntries.ContainsKey(userId))
-            HistoryEntries.Add(userId, new Dictionary<string, HistoryEntry>());
-        HistoryEntries[userId][historyEntry.Function] = historyEntry;
+            HistoryEntries.TryAdd(userId, new ConcurrentDictionary<string, HistoryEntry>());
+        if (HistoryEntries.TryGetValue(userId, out var userHistory))
+            userHistory.AddOrUpdate(historyEntry.Function, historyEntry, (_, _) => historyEntry);
     }
 
-    public List<HistoryEntry> Get(int userId) =>
-        !HistoryEntries.ContainsKey(userId) ? new List<HistoryEntry>() : HistoryEntries[userId].Values.ToList();
+    public List<HistoryEntry> Get(int userId)
+    {
+        if (!HistoryEntries.ContainsKey(userId))
+            return new List<HistoryEntry>();
+        
+        return HistoryEntries.TryGetValue(userId, out var userHistory)
+            ? userHistory.Values.ToList()
+            : new List<HistoryEntry>();
+    }
 }
